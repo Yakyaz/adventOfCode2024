67891346c49380cdf0242c81a3ae9baf274f221c
Yakyaz <contact@yakyaz.dev>
2024-12-07 18:48:56 +0100

    day 7

---
+++ day7/task2.py
@@ -0,0 +1,41 @@
+input = open("input.txt")
+
+lines = input.readlines()
+
+
+
+lists = []
+for line in lines:
+    line = line.strip()
+    key, values = line.split(':', 1)  # Teilt in zwei Teile
+    # Speichere den Schlüssel und die Zahlen als eine Liste innerhalb einer Liste
+    lists.append([int(key)] + list(map(int, values.strip().split())))
+
+
+result = 0
+
+def evaluate_all_combinations(numbers, current_result, index, key):
+    if index == len(numbers):  
+        return current_result == key
+
+   
+    if evaluate_all_combinations(numbers, current_result + numbers[index], index + 1, key):
+        return True
+
+
+    if evaluate_all_combinations(numbers, current_result * numbers[index], index + 1, key):
+        return True
+    
+    concatenated_result = int(str(current_result) + str(numbers[index]))
+    if evaluate_all_combinations(numbers, concatenated_result, index + 1, key):
+        return True
+    
+# Jede Liste prüfen
+for lst in lists:
+    key = lst[0] 
+    numbers = lst[1:]  # Die Zahlen (Werte)
+    if evaluate_all_combinations(numbers, numbers[0], 1, key):
+        result += key
+
+
+print(result)
